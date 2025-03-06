189e7185fa48347aa1943c0fdce80b3ae54f03aa
Неминущий Максим Иванович <neminuschiy@DRAFT.LOCAL>
2025-03-06 16:06:31 +0300

    first commit

--- src/components/shop/shopModel.tsx
+++ src/components/shop/shopModel.tsx
@@ -3,27 +3,29 @@ import './shopModal.css';
 
 interface ShopModalProps {
     onClose: () => void;
-    isOpen: boolean; // Теперь принимаем состояние открытия извне
+    isOpen: boolean;
   }
-  
+
   const ShopModal: React.FC<ShopModalProps> = ({ onClose, isOpen }) => {
     const [isAnimated, setIsAnimated] = useState(false);
-  
+
     useEffect(() => {
       if (isOpen) {
-        // Через 0.1 секунды после открытия запускаем анимацию
         const timeout = setTimeout(() => setIsAnimated(true), 100);
         return () => clearTimeout(timeout);
       } else {
-        setIsAnimated(false); // Сброс анимации при закрытии
+        setIsAnimated(false);
       }
-    }, [isOpen]);   
-  
+    }, [isOpen]);
+
     return (
-      <div className={`modal ${isAnimated ? 'active' : ''}`}
-           style={{ display: isOpen ? 'flex' : 'none' }}
+      <div
+        className={`modal ${isAnimated ? 'active' : ''}`}
+        style={{ display: isOpen ? 'flex' : 'none' }}
       >
-        <div className="modal_content">
+        <div
+          className="modal-content"
+        >
           <h2>Магазин</h2>
           <p>Содержимое магазина...</p>
           <button onClick={onClose}>Закрыть</button>
